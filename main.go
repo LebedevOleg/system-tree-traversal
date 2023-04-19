e5c316dfa0e8df897006488b9db029abf8dbe22a
LebedevOleg <56258311+LebedevOleg@users.noreply.github.com>
2023-04-19 11:30:06 +0300

    Update func

--- main.go
+++ main.go
@@ -2,98 +2,166 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
 	"os"
-	"runtime"
+	"path/filepath"
 	"strconv"
 	"sync"
 	"time"
 )
 
-func Walk(file *os.File, path string, m *sync.Mutex, wg *sync.WaitGroup) error {
-	dir, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer dir.Close()
-	data, err := dir.Readdir(-1)
-	if err != nil {
-		return err
+// Функция с Асинхронным обходом файлов дериктории
+// file - файл в который будет вестись запись, path - путь к дериктории которую будем обходить
+// workers - максимальное количество горутин на обход дериктории
+func WalkAsync(file *os.File, path string, workers int) error {
+	// создаем канал для записи в файл
+	resChan := make(chan string)
+	var wg sync.WaitGroup
+	// запускаем множество горутин записи в файл
+	//* скорее всего операция бессмысленная поскольку в момент времени 2 горутины не могут писать в файл
+	//* с другой стороны это необходимо чтобы канал на запись не заблокировался
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for res := range resChan {
+				_, err := file.WriteString(res)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
 	}
 
-	for _, d := range data {
-		if !d.IsDir() {
-			m.Lock()
-			_, err = file.WriteString(dir.Name() + "; " + d.Name() + "; " + strconv.FormatInt(d.Size(), 10) + "\n")
-			m.Unlock()
-			if err != nil {
-				return err
-			}
-			continue
-		}
-		wg.Add(1)
-		go Walk(file, path+"/"+d.Name(), m, wg)
-		if err != nil {
-			return err
-		}
-	}
-	wg.Done()
+	wg.Add(1)
+	var wg2 sync.WaitGroup
+	workChan := make(chan int, workers)
+	// запускаем множество горутин обхода дериктории
+	// чтобы они не завершились раньше времени, пришлось добавить второй объект waitGroup
+	// и дополнительный канал для работающих горутин, чтобы не выйти за пределы работающих потоков
+	go func(wg2 *sync.WaitGroup) {
+		defer wg.Done()
+		wg2.Add(1)
+		workChan <- 1
+		walkDir(path, resChan, wg2, workChan)
+		wg2.Wait()
+		close(resChan)
+	}(&wg2)
+	wg.Wait()
+	close(workChan)
 	return nil
 }
 
-func Walksync(file *os.File, path string) error {
-	dir, err := os.Open(path)
+func walkDir(dir string, fileChan chan<- string, wg *sync.WaitGroup, workChan chan int) {
+	defer wg.Done()
+
+	entries, err := ioutil.ReadDir(dir) // Прочитываем все список содержимого каталога
 	if err != nil {
-		return err
-	}
-	defer dir.Close()
-	data, err := dir.Readdir(-1)
-	if err != nil {
-		return err
+		fmt.Println(err)
+		return
 	}
 
-	for _, d := range data {
-		if !d.IsDir() {
-			_, err = file.WriteString(dir.Name() + "; " + d.Name() + "; " + strconv.FormatInt(d.Size(), 10) + "\n")
-			if err != nil {
-				return err
-			}
+	for _, entry := range entries {
+		//! я делал проверку работы алгоритма на съемном HDD
+		//! обработать системный файл не получалось из-за недостатка прав
+		if entry.Name() == "System Volume Information" {
 			continue
 		}
-		Walksync(file, path+"/"+d.Name())
-		if err != nil {
-			return err
+		// если рассматриваемый объект является дерикторией
+		if entry.IsDir() {
+			path := filepath.Join(dir, entry.Name())
+			wg.Add(1)
+			// проверяем переполнен ли пул рабочих потоков
+			// если нет, запускаем асинхронный обход дериктории
+			// если да, запускаем в этой же горутине обход дериктории
+			select {
+			case workChan <- 1:
+				go walkDir(path, fileChan, wg, workChan) // Запускаем обход поддиректории в отдельной горутине
+			default:
+				walkDir(path, fileChan, wg, workChan) // Запускаем обход поддиректории в отдельной горутине
+			}
+		} else {
+			// если рассматриваемый объект является файлом - записываем его данные в файл
+			fileChan <- dir + "; " + entry.Name() + "; " + strconv.FormatInt(entry.Size(), 10) + "\n" // Отправляем путь файла в канал
 		}
 	}
+	// по окончанию обхода читаем из пула работников значение для освобождения места под параллельный процесс
+	select {
+	case <-workChan:
+		return
+	default:
+		return
+	}
+
+}
+
+// Функция с Последовательным обходом файлов дериктории
+// file - файл в который будет вестись запись, path - путь к дериктории которую будем обходить
+func WalkPath(file *os.File, path string) error {
+	dataChan := make(chan string)
+	var wg sync.WaitGroup
+	// аналогично Асинхронному методу создаем горктины на запись
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for fileData := range dataChan {
+				_, err := file.WriteString(fileData)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
+	}
+	// с помощью библитеки filepath обходим дерикторию в ложенные подпапки
+	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		//если рассматриваемый обЪект является дерикторией
+		// просто идем дальше, поскольку метод Walk() сам обходит дериктории, ему не нужны указания на это
+		if info.IsDir() {
+			return nil
+		}
+		// если рассматриваемый объект является файлом - записываем результат
+		dataChan <- path + "; " + info.Name() + "; " + strconv.FormatInt(info.Size(), 10) + "\n"
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	close(dataChan)
+	wg.Wait()
 	return nil
 }
 
 func main() {
-	path := "H:/" // Записываем путь к интерисующей дериктории
-	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	path := "H:/"                                                                   // Записываем путь к интерисующей дериктории
+	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600) // открываем файл для записи
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	//---------------------------
-	/* t := time.Now()
-	fmt.Println(t)
-	err = Walksync(file, path)
+	//---------------------------Последовательный обход
+	t5 := time.Now()
+	fmt.Println(t5)
+	err = WalkPath(file, path)
 	if err != nil {
 		panic(err)
 	}
-	t2 := time.Now()
-	fmt.Println(t2.Sub(t)) */
-	//---------------------------
-	runtime.GOMAXPROCS(1000)
-	var m sync.Mutex
-	var wg sync.WaitGroup
-	t3 := time.Now()
-	fmt.Println(t3)
-	err = Walk(file, path, &m, &wg)
-	wg.Wait()
+	t6 := time.Now()
+	fmt.Println(t6.Sub(t5))
+	//---------------------------Попытка в асинхронный обход
+	t7 := time.Now()
+	fmt.Println(t7)
+	err = WalkAsync(file, path, 100)
 	if err != nil {
 		panic(err)
 	}
-	t4 := time.Now()
-	fmt.Println(t4.Sub(t3))
+	t8 := time.Now()
+	fmt.Println(t8.Sub(t7))
+	//! Всего файлов в моем HDD набралось 215506
+	//! среднее время работы:
+	//! windows -
+	//! linux -
+	return
 }
